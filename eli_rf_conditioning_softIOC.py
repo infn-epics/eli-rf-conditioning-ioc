6c2dab17f388e464dc3ab4ecafa75021ba0a5e00
serenell <beatrice.serenellini@lnf.infn.it>
2026-02-04 11:41:48 +0100

    waveform interlock added

--- eli_rf_conditioning_softIOC.py
+++ eli_rf_conditioning_softIOC.py
@@ -25,9 +25,6 @@ with open(args.conf, 'r') as f:
 # Load prefixes from config file
 prefix_rf_conditioning=config.get('prefix_rf_conditioning')
 prefix_LLRF=config.get('prefix_LLRF')
-# Linea1 "W1DSIP01","W1GSIP01","W1KSIP01","W1KSIP02","W1KSIP03","GUNSIP00","GUNSIP01","GUNSIP02","RFDSIP01"
-# pv_laser_amp_llrf=config.get('pv_laser_amp_llrf')
-# prefix_redpitaya02=config.get('prefix_redpitaya02')
 print("\nList of epics prefixes:")
 print(prefix_rf_conditioning)
 print(prefix_LLRF)
@@ -42,7 +39,7 @@ for item in config.get('pv_to_build', []):
             globals()[item['pvname'].lower()]=func(item['pvname'], initial_value=int(item['init_value']))
         elif item['type']=="WaveformOut":
             globals()[item['pvname'].lower()]=func(item['pvname'], initial_value=list(item['init_value']))
-        elif item['type']=="stringIn":
+        elif item['type'] in ["stringIn", "stringOut"]:
             globals()[item['pvname'].lower()]=func(item['pvname'], initial_value=str(item['init_value']))
         else:
             globals()[item['pvname'].lower()]=func(item['pvname'], initial_value=float(item['init_value']))
@@ -59,95 +56,132 @@ softioc.iocInit()
 # Start processes required to be run after iocInit
 
 def main(prefix_rf_conditioning: str, prefix_LLRF: str):
-    
-    # Init variables
-    loop_period=0.1 # Pause in seconds between iterations
-    print(vacuum_pumps.get())
+    loop_period = 0.1
     vacuum_over_tsh = False
-    #print(caget(prefix_pumps.get()[0]+vacuum_pumps.get()[0]+suffix_pumps.get()[0]))
-    #print(caget(prefix_pumps.get()[1]+vacuum_pumps.get()[1]+suffix_pumps.get()[1]))
-    # Starting main loop
+
     while True:
 
-        # WORKS WITH FEEDBACK ALWAYS ON
-        # DEFINE INITIAL SETPIOINT
-        if caget(prefix_LLRF+":app:rf_ctrl") == False and conditioning_status.get()==True:
-            #conditioning_status.set(False)
-            #counter_intlk.set(0)
-            # c'è anche counter_intlk_tsh che viene creata nel json ma non più usata
+        if caget(prefix_LLRF + ":app:rf_ctrl") == False and conditioning_status.get() == True:
             raise_count.set(0)
         else:
             pass
-        if power_raise_status.get()==0 and raise_count.get()!=0: # raise_count increase only when power_raise_status=1
+
+        if power_raise_status.get() == 0 and raise_count.get() != 0:
             raise_count.set(0)
 
-        # Check vacuum thresholds and perform conditioning actions
         if conditioning_status.get() == True:
-            for i in range(len(vacuum_pumps.get())):
-                if caget(prefix_pumps.get()[i]+vacuum_pumps.get()[i]+suffix_pumps.get()[i])>vacuum_tsh.get()[i]:
-                    vac_id.set(i+1) # at least one pump exceded tsh
-                    break
-                else: vac_id.set(0)
 
-            # First intlk event above threshold, turn off RF and save info
-            if vac_id.get()!=0 and vacuum_over_tsh==False:
+            for i in range(len(vacuum_pumps.get())):
+                if caget(prefix_pumps.get()[i] + vacuum_pumps.get()[i] + suffix_pumps.get()[i]) > vacuum_tsh.get()[i]:
+                    vac_id.set(i + 1)
+                    break
+                else:
+                    vac_id.set(0)
+
+            if vac_id.get() != 0 and vacuum_over_tsh == False:
                 power_raise_status.set(0)
-                llrf_fbk_level=caget(prefix_LLRF+":vm:dsp:sp_amp:power")
-                caput(prefix_LLRF+":app:rf_ctrl", "0")
-                conditioning_setpoint.set(llrf_fbk_level*1e-6*intlk_hysteresis.get())
-                last_intlk_source.set(vacuum_pumps.get()[i]) # stores last intlk source
-                last_intlk_datetime.set(str(time.time())) #store last intlk time
+                llrf_fbk_level = caget(prefix_LLRF + ":vm:dsp:sp_amp:power")
+                caput(prefix_LLRF + ":app:rf_ctrl", "0")
+                conditioning_setpoint.set(llrf_fbk_level * 1e-6 * intlk_hysteresis.get())
+                last_intlk_source.set(vacuum_pumps.get()[i])
+                last_intlk_datetime.set(str(time.time()))
                 vacuum_over_tsh = True
 
-            # All pumps below threshold after an intlk event, restart reduced power
             elif vac_id.get() == 0 and vacuum_over_tsh == True:
                 power_raise_status.set(1)
-                caput(prefix_LLRF + ":vm:dsp:sp_amp:power", str(conditioning_setpoint.get() * 1e6)) # set power
-                caput(prefix_LLRF+":app:rf_ctrl", "1") # RF ON
-                caput(prefix_LLRF+":vm:dsp:pi_amp:loop_closed", "1") # AMP FBK ON
-                vacuum_over_tsh = False 
+                caput(prefix_LLRF + ":vm:dsp:sp_amp:power", str(conditioning_setpoint.get() * 1e6))
+                caput(prefix_LLRF + ":app:rf_ctrl", "1")
+                caput(prefix_LLRF + ":vm:dsp:pi_amp:loop_closed", "1")
+                vacuum_over_tsh = False
 
-            # All pumps below threshold, increase the power
             elif vac_id.get() == 0 and vacuum_over_tsh == False:
-                pass # No action, but included for completeness
+                pass
 
-            # Power raise logic
-            if power_raise_status.get() == True and caget(prefix_LLRF+":vm:dsp:sp_amp:power")<=(conditioning_target.get()*1e6): # power increase
-                if raise_count.get() % (power_raise_wait.get()*600) == 0 and raise_count.get()!=0 : # the time interval for power increase is set in minutes  
-                    llrf_fbk_level=caget(prefix_LLRF+":vm:dsp:sp_amp:power")
-                    caput(prefix_LLRF+":vm:dsp:sp_amp:power", str(llrf_fbk_level+power_raise_step.get()*1e3))
+            if power_raise_status.get() == True and caget(prefix_LLRF + ":vm:dsp:sp_amp:power") <= (conditioning_target.get() * 1e6):
+                if raise_count.get() % (power_raise_wait.get() * 600) == 0 and raise_count.get() != 0:
+                    llrf_fbk_level = caget(prefix_LLRF + ":vm:dsp:sp_amp:power")
+                    caput(prefix_LLRF + ":vm:dsp:sp_amp:power", str(llrf_fbk_level + power_raise_step.get() * 1e3))
                     raise_count.set(0)
+                raise_count.set(raise_count.get() + 1)
 
-                raise_count.set(raise_count.get()+1)
- 
-            #     if counter_intlk.get() == 0: # first interlock of the series
-            #         llrf_fbk_level=caget(prefix_LLRF+":vm:dsp:sp_amp:power")
-            #         caput(prefix_LLRF+":vm:dsp:sp_amp:power", str(0)) # feedback setpoint = 0
-            #         conditioning_setpoint.set(llrf_fbk_level*1e-6*intlk_hysteresis.get())# conditioning setpoint = llrf setpoint * hysteresis
-            #         counter_intlk.set(counter_intlk.get()+1) # increase interlock counter
-            #         last_intlk_source.set(vacuum_pumps.get()[i]) # stores last intlk source
-            #         last_intlk_datetime.set(str(time.time())) #store last intlk time
-            #     elif counter_intlk.get() >= counter_intlk_tsh.get(): #check if the consecutive interlocks exceed the threshold
-            #         caput(prefix_LLRF+":app:rf_ctrl", "0") # turn OFF RF
-            #         counter_intlk.set(0)
-            #         caput(prefix_LLRF+":vm:dsp:sp_amp:power", str(conditioning_setpoint.get()*1e6))  # to verify
-            #     elif counter_intlk.get() != 0:
-            #         counter_intlk.set(counter_intlk.get()+1) # increase interlock counter
-            # elif counter_intlk.get() != 0:  # vacuum has re-entered safe zone
-            #     caput(prefix_LLRF+":vm:dsp:sp_amp:power", str(conditioning_setpoint.get()*1e6)) # feedback setpoint = conditioning setpoint (previously decrease by hysteresis)
-            #     counter_intlk.set(0)
-            #     raise_count.set(0)
-              
-            # elif power_raise_status.get() == True and caget(prefix_LLRF+":vm:dsp:sp_amp:power")<=(conditioning_target.get()*1e6): # power increase
-            #     if raise_count.get() % (power_raise_wait.get()*60) == 0 and raise_count.get()!=0 : # the time interval for power increase is set in minutes  
-            #         llrf_fbk_level=caget(prefix_LLRF+":vm:dsp:sp_amp:power")
-            #         caput(prefix_LLRF+":vm:dsp:sp_amp:power", str(llrf_fbk_level+power_raise_step.get()*1e3))
-            #         raise_count.set(0)
+        # ===============================
+        # Pulse-to-pulse waveform monitor
+        # ===============================
 
-            #     raise_count.set(raise_count.get()+1)
-         
-        # Pause for loop period
-        cothread.Sleep(loop_period)
+            wf_pv_name = prefix_LLRF + ":" + wf_source_suffix.get()
+            wf_time_pv = prefix_LLRF + ":app:time_vector"
+
+            if wf_pulse_intlk_enable.get() == 0:
+                wf_interlock.set(0)
+                cothread.Sleep(loop_period)
+                continue
+
+            wf_curr = caget(wf_pv_name)
+            time_vec = caget(wf_time_pv)
+
+            if wf_prev_valid.get() == 0:
+                wf_pulse_prev.set(list(wf_curr))
+                wf_prev_valid.set(1)
+                wf_interlock.set(0)
+                cothread.Sleep(loop_period)
+                continue
+
+            t_start = wf_offset_curs_us.get()
+            t_stop = t_start + wf_duration_curs_us.get()
+            i_start = None
+            i_stop = None
+
+            for i, t in enumerate(time_vec):
+                if i_start is None and t >= t_start:
+                    i_start = i
+                if t >= t_stop:
+                    i_stop = i
+                    break
+
+            if i_start is None or i_stop is None or i_stop <= i_start:
+                wf_interlock.set(0)
+                cothread.Sleep(loop_period)
+                continue
+
+            wf_prev = wf_pulse_prev.get()
+            tol = wf_mask_percent.get() / 100.0
+            fault = False
+
+            for i in range(i_start, i_stop):
+                ref = wf_prev[i]
+                low = ref * (1 - tol)
+                high = ref * (1 + tol)
+                if wf_curr[i] < low or wf_curr[i] > high:
+                    fault = True
+                    break
+
+            if not fault:
+                wf_pulse_prev.set(list(wf_curr))
+
+            wf_interlock.set(1 if fault else 0)
+
+            # ===============================
+            # Combine vacuum and waveform interlocks
+            # ===============================
+
+            interlock_active = vac_id.get() != 0 or wf_interlock.get() == 1
+
+            if interlock_active and vacuum_over_tsh == False:
+                power_raise_status.set(0)
+                llrf_fbk_level = caget(prefix_LLRF + ":vm:dsp:sp_amp:power")
+                caput(prefix_LLRF + ":app:rf_ctrl", "0")
+                conditioning_setpoint.set(llrf_fbk_level * 1e-6 * intlk_hysteresis.get())
+
+                if vac_id.get() != 0:
+                    last_intlk_source.set(vacuum_pumps.get()[vac_id.get() - 1])
+                elif wf_interlock.get() == 1:
+                    last_intlk_source.set("WF_PULSE_TO_PULSE")
+
+                last_intlk_datetime.set(str(time.time()))
+                vacuum_over_tsh = True
+    
+            # Pause for loop period
+            cothread.Sleep(loop_period)
 
 ## dump pvs
 softioc.dbl()
